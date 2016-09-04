ecc910cc0c3fa348602e21250ccc2d2266647ac0
naieem <naieemsupto@gmail.com>
2016-09-04 18:45:38 +0600

    new update from 4092016

--- plugin-login-registration/mentor/nt_handle_ajax.php
+++ plugin-login-registration/mentor/nt_handle_ajax.php
@@ -11,17 +11,24 @@ class nt_handle_ajax {
 
     //construct
     public function __construct() {
+        // logo uploading ajax
         add_action('wp_ajax_upload_logo', array($this, 'logo_upload'));
         add_action('wp_ajax_nopriv_upload_logo', array($this, 'logo_upload'));
-
+        // Banner Update ajax
         add_action('wp_ajax_banner_update', array($this, 'update_banner_part'));
         add_action('wp_ajax_nopriv_banner_update', array($this, 'update_banner_part'));
-
+        // Why update ajax
         add_action('wp_ajax_why_update', array($this, 'update_why'));
         add_action('wp_ajax_nopriv_why_update', array($this, 'update_why'));
+        // attachment delete ajax
+        add_action('wp_ajax_delete_attachement', array($this, 'attachment_delete'));
+        add_action('wp_ajax_nopriv_delete_attachement', array($this, 'attachment_delete'));
+        /* icon upload */
+        add_action('wp_ajax_icon_upload', array($this, 'upload_icon'));
+        add_action('wp_ajax_nopriv_icon_upload', array($this, 'upload_icon'));
     }
 
-    //start method chaining
+    // logo uploading ajax
     public function logo_upload() {
 
         require_once( ABSPATH . 'wp-admin/includes/image.php' );
@@ -59,6 +66,7 @@ class nt_handle_ajax {
         die();
     }
 
+    // Banner Update ajax    
     public function update_banner_part() {
         require_once( ABSPATH . 'wp-admin/includes/image.php' );
         require_once( ABSPATH . 'wp-admin/includes/file.php' );
@@ -73,26 +81,114 @@ class nt_handle_ajax {
         die();
     }
 
+    // Why update ajax
     public function update_why() {
-//        require_once( ABSPATH . 'wp-admin/includes/image.php' );
-//        require_once( ABSPATH . 'wp-admin/includes/file.php' );
-//        require_once( ABSPATH . 'wp-admin/includes/media.php' );
-//        $newupload = media_handle_upload('banner_file', $_POST['id']);
+        $data = array();
+        require_once( ABSPATH . 'wp-admin/includes/image.php' );
+        require_once( ABSPATH . 'wp-admin/includes/file.php' );
+        require_once( ABSPATH . 'wp-admin/includes/media.php' );
+        $newupload = media_handle_upload('why_file', $_POST['id']);
 //        update_post_meta($_POST['id'], 'banner_title', $_POST['banner_title']);
 //        update_post_meta($_POST['id'], 'banner_sub_title', $_POST['banner_sub_title']);
 //        update_post_meta($_POST['id'], 'banner_bg', wp_get_attachment_url($newupload));
 //        echo $_POST['banner_title'] . "<br>";
 //        echo $_POST['banner_sub_title'] . "<br>";
-//        echo wp_get_attachment_url($newupload);
+
         if (isset($_POST['why_title'])) {
             $why_title = $_POST['why_title'];
-            update_post_meta($post_id, 'why_title', $why_title);
+            update_post_meta($_POST['id'], 'why_title', $why_title);
         }
 
-        if (isset($_POST['why_points'])){
+        if (isset($_POST['why_points'])) {
             $why_points = $_POST['why_points'];
-            update_post_meta($post_id, 'why_points', $why_points);
+            update_post_meta($_POST['id'], 'why_points', $why_points);
         }
+
+        if (isset($newupload)) {
+            update_post_meta($_POST['id'], 'why_bg', wp_get_attachment_url($newupload));
+        }
+        $data['image_id'] = $newupload;
+        $data['image_url'] = wp_get_attachment_url($newupload);
+        echo json_encode($data);
+        die();
+    }
+
+    // attachment_delete ajax    
+    public function attachment_delete() {
+        $data = array();
+        if (false === wp_delete_attachment($_POST['id']) || $_POST['id'] == '') {
+            $data['message'] = "Attachment Not deleted";
+            $data['result'] = false;
+        } else {
+            $data['message'] = "Attachment deleted succesfully";
+            $data['result'] = true;
+            update_post_meta($_POST['post_id'], 'why_bg', "");
+        }
+        echo json_encode($data);
+        die();
+    }
+
+    /* #######
+     * Getting image name from image url
+      ####### */
+
+    public function get_image_name($url) {
+        $url_arr = explode('/', $url);
+        $ct = count($url_arr);
+        $name = $url_arr[$ct - 1];
+        $name_div = explode('.', $name);
+        $ct_dot = count($name_div);
+        $img_name = $name_div[$ct_dot - $ct_dot];
+        return $img_name;
+    }
+
+    // attachment_delete ajax    
+    public function upload_icon() {
+        $icons = array();
+        $data = array();
+        require_once( ABSPATH . 'wp-admin/includes/image.php' );
+        require_once( ABSPATH . 'wp-admin/includes/file.php' );
+        require_once( ABSPATH . 'wp-admin/includes/media.php' );
+        if (!empty($_FILES['department_icon']['name'])) {
+            $newupload = media_handle_upload('department_icon', $_POST['id']);
+            /* ######## pushign to array */
+            if (count($_POST['dept_icons']) <= 0) {
+                array_push($icons, wp_get_attachment_url($newupload));
+            } else {
+                $icons = $_POST['dept_icons'];
+                array_push($icons, wp_get_attachment_url($newupload));
+            }
+            /* loop for getting url and name */
+            $c = 0;
+            foreach ($icons as $icon) {
+                $data[$c]['url'] = $icon;
+                $data[$c]['name'] = $this->get_image_name($icon);
+                $c = $c + 1;
+            }
+            /* end naming and url getting */
+            update_post_meta($_POST['id'], 'dept_icons', $icons); //updating post meta
+            echo json_encode($data); // returning json value to view
+        } else {
+            /* ######## pushign to array */
+            if (count($_POST['dept_icons']) <= 0) {
+                //array_push($icons, wp_get_attachment_url($newupload));
+            } else {
+                $icons = $_POST['dept_icons'];
+                //array_push($icons, wp_get_attachment_url($newupload));
+            }
+            /* loop for getting url and name */
+            $c = 0;
+            foreach ($icons as $icon) {
+                $data[$c]['url'] = $icon;
+                $data[$c]['name'] = $this->get_image_name($icon);
+                $c = $c + 1;
+            }
+            /* end naming and url getting */
+            update_post_meta($_POST['id'], 'dept_icons', $icons); //updating post meta
+            echo json_encode($data); // returning json value to view
+        }
+        //var_dump($_FILES);
+
         die();
     }
 
--- plugin-login-registration/mentor/nt_meta_box.php
+++ plugin-login-registration/mentor/nt_meta_box.php
@@ -18,17 +18,20 @@ class nt_meta_box {
     public function custom_meta_box() {
         add_meta_box("price_meta_id", "Price fields :", array($this, 'price_meta'), "page", "normal", "low");
         //add_meta_box("logo_img_meta_id", "Backgimd :", array($this, 'bg_image'), "page", "normal", "low");
-        add_meta_box("bg_img_meta_id", "Images for the page:", array($this, 'bg_image'), "page", "normal", "low");
+        add_meta_box("bg_img_meta_id", "Logo images for the page:", array($this, 'logo_image'), "page", "normal", "low");
         add_meta_box("banner_meta", "Banner part:", array($this, 'banner_part'), "page", "normal", "low");
         add_meta_box("why_meta", "Why choose part:", array($this, 'why_part'), "page", "normal", "low");
+        add_meta_box("department_meta", "Department part:", array($this, 'department_part'), "page", "normal", "low");
     }
+
+    /* Why part */
 
     public function why_part() {
         global $post;
         $why_title = get_post_meta($post->ID, "why_title", true);
         $why_points = get_post_meta($post->ID, "why_points", true);
         $why_bg = get_post_meta($post->ID, "why_bg", true);
-        print_r($why_points);
+        //print_r($why_points);
         echo '<label>Title :</label><input type="text" style="width:100%;" name="why_title" size="10" value="' . $why_title . '"/>';
         echo '<div class="point"><label>Points :</label><br>';
         if (is_array($why_points)) {
@@ -40,9 +43,9 @@ class nt_meta_box {
         } else {
             echo '<input type="text" style="width:80%;" name="why_points[]" value=""/><span class="remove_why button button-primary button-large">Remove</span></li>';
         }
-        echo '</div><br><label>Banner Background:</label><input type="text" style="width:100%;" name="why_bg" value="' . $why_bg . '"/>';
-
-        echo '<span class="add_why button button-primary button-large">' . __('Add why') . '</span>';
+        echo "</div>";
+        echo '<br><span class="add_why button button-primary button-large">' . __('Add why') . '</span><br>';
+        echo '<br><label>Banner Background:</label><input type="text" style="width:100%;" name="why_bg" value="' . $why_bg . '"/>';
         ?>
         <script>
             $ = jQuery.noConflict();
@@ -62,6 +65,96 @@ class nt_meta_box {
         <?php
     }
 
+    /* Department part */
+
+    public function department_part() {
+        global $post;
+        $dept_title = get_post_meta($post->ID, "dept_title", true);
+        $dept_bg = get_post_meta($post->ID, "dept_bg", true);
+        $dept_single = get_post_meta($post->ID, "dept_single", true);
+        $dept_icons = get_post_meta($post->ID, "dept_icons", true);
+        /* Single department adding */
+        echo '<label>Title :</label><input type="text" style="width:100%;" name="dept_title" size="10" value="' . $dept_title . '"/>';
+        echo '<div class="department_container"><br><b>Single Department :</b><br>';
+        echo '<ul id="dept_single">';
+        $c = 0;
+        if (is_array($dept_single)) {
+            if (count($dept_single) > 0) {
+                foreach ($dept_single as $depts) {
+                    //echo $value;
+                    echo '<li><hr><label>Title:</label><input type="text" style="width:100%;" name="dept_single[' . $c . '][title]" value="' . $depts['title'] . '"/>';
+                    echo '<label>SubTitle:</label><input type="text" style="width:100%;" name="dept_single[' . $c . '][subtitle]" value="' . $depts['subtitle'] . '"/>';
+                    echo '<label>Image:</label><input type="text" style="width:100%;" name="dept_single[' . $c . '][img]" value="' . $depts['img'] . '"/>';
+                    echo "<span class='remove_dept button button-primary button-large'>Remove</span><hr></li>";
+                    $c = $c + 1;
+                }
+            }
+        } else {
+            echo '<li><hr><label>Title:</label><input type="text" style="width:100%;" name="dept_single[' . $c . '][title]" value="" />';
+            echo '<label>SubTitle:</label><input type="text" style="width:100%;" name="dept_single[' . $c . '][subtitle]" value="" />';
+            echo '<label>Image:</label><input type="text" style="width:100%;" name="dept_single[' . $c . '][img]" value="" />';
+            echo "<span class='remove_dept button button-primary button-large'>Remove</span><hr></li>";
+            $c = $c + 1;
+        }
+        echo '</ul>';
+
+        echo "</div>";
+        echo '<br><span class="add_dept button button-primary button-large">' . __('Add new Department') . '</span><br>';
+        echo '<br><label>Department Background:</label><input type="text" style="width:100%;" name="dept_bg" value="' . $dept_bg . '"/>';
+        echo "<br><b>Icon list</b></br>";
+        /* Adding new icons in the department */
+        echo "<ul class='dept_icon'>";
+        if (is_array($dept_icons)) {
+            if (count($dept_icons) > 0) {
+                foreach ($dept_icons as $icons) {
+                    echo '<li><input type="hidden" style="width:100%;" name="dept_icons[]" value="' . $icons . '" />';
+                    echo "<img src='".$icons."' height='200' width='200'><br>";
+                    echo "<span class='remove_icon button button-primary button-large'>Remove</span></li>";
+                }
+            }
+        } else {
+            echo '<li><label>Icons Url:</label><input type="text" style="width:100%;" name="dept_icons[]" value="" /><br>';
+            echo "<span class='remove_icon button button-primary button-large'>Remove</span></li>";
+        }
+        echo "</ul>";
+
+        echo '<br><span class="add_icon button button-primary button-large">' . __('Add new icon') . '</span><br>';
+        ?>
+        <script>
+            $ = jQuery.noConflict();
+            $(document).ready(function () {
+                /* Addign single department information */
+                var c =<?php echo $c; ?>;
+                $(".add_dept").click(function (e) {
+                    e.preventDefault();
+                    $("#dept_single").append('<li><hr><label>Title:</label><input type="text" style="width:100%;" name="dept_single[' + c + '][title]" value=""/>' +
+                            '<label>SubTitle:</label><input type="text" style="width:100%;" name="dept_single[' + c + '][subtitle]" value=""/>' +
+                            '<label>Image:</label><input type="text" style="width:100%;" name="dept_single[' + c + '][img]" value=""/>' +
+                            '<span class="remove_dept button button-primary button-large">Remove</span>' +
+                            '<hr></li>');
+                    c = c + 1;
+                });
+                $(".remove_dept").live('click', function () {
+                    $(this).parent().remove();
+                });
+
+
+                /* Adding new icons url */
+                $(".add_icon").click(function (e) {
+                    e.preventDefault();
+                    $(".dept_icon").append('<li><label>Icons Url:</label><input type="text" style="width:100%;" name="dept_icons[]" value="" /><br>' +
+                            '<span class="remove_icon button button-primary button-large">Remove</span></li>');
+                });
+                $(".remove_icon").live('click', function () {
+                    $(this).parent().remove();
+                });
+            });
+        </script>
+        <?php
+    }
+
+    /* Banner part */
+
     public function banner_part() {
         global $post;
         $banner_title = get_post_meta($post->ID, "banner_title", true);
@@ -72,7 +165,9 @@ class nt_meta_box {
         echo '<label>Banner Background:</label><input type="text" style="width:100%;" name="banner_bg" size="10" value="' . $banner_bg . '"/>';
     }
 
-    public function bg_image() {
+    /* Logo part */
+
+    public function logo_image() {
         global $post;
         $data = get_post_meta($post->ID, "logo_img", true);
         echo '<label>Logo :</label><input type="text" style="width:100%;" name="logo_img" size="10" value="' . $data . '"/>';
@@ -140,6 +235,7 @@ class nt_meta_box {
             $price_data = $_POST['price_data'];
             update_post_meta($post_id, 'price_data', $price_data);
         }
+        /* Banner meta update */
         if (isset($_POST['banner_title'])) {
             $banner_title = $_POST['banner_title'];
             update_post_meta($post_id, 'banner_title', $banner_title);
@@ -154,6 +250,7 @@ class nt_meta_box {
             update_post_meta($post_id, 'banner_bg', $banner_bg);
         }
 
+        /* Why meta update */
         if (isset($_POST['why_title'])) {
             $why_title = $_POST['why_title'];
             update_post_meta($post_id, 'why_title', $why_title);
@@ -166,6 +263,25 @@ class nt_meta_box {
         if (isset($_POST['why_bg'])) {
             $why_bg = $_POST['why_bg'];
             update_post_meta($post_id, 'why_bg', $why_bg);
+        }
+
+        /* Department meta update */
+        if (isset($_POST['dept_title'])) {
+            $dept_title = $_POST['dept_title'];
+            update_post_meta($post_id, 'dept_title', $dept_title);
+        }
+
+        if (isset($_POST['dept_bg'])) {
+            $dept_bg = $_POST['dept_bg'];
+            update_post_meta($post_id, 'dept_bg', $dept_bg);
+        }
+        if (isset($_POST['dept_single'])) {
+            $dept_single = $_POST['dept_single'];
+            update_post_meta($post_id, 'dept_single', $dept_single);
+        }
+        if (isset($_POST['dept_icons'])) {
+            $dept_icons = $_POST['dept_icons'];
+            update_post_meta($post_id, 'dept_icons', $dept_icons);
         }
     }
 
